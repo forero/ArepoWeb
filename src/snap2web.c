fbb36184f3639d68d11d0481a3c04e7278348223
Jamie <j.e.forero.romero@gmail.com>
2014-06-12 10:29:24 +0200

    computes the eigenvalues and eigenvectors

--- src/snap2web.c
+++ src/snap2web.c
@@ -1,11 +1,23 @@
 #include <hdf5.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #define DM_COORDINATES "/PartType1/Coordinates"
 #define DM_VELOCITIES "/PartType1/Velocities"
 #define GAS_GRADIENT "/PartType0/VelocityGradient"
 #define USAGE "./snap2web.x snapfilename"
+#define XX  0
+#define XY  1
+#define XZ  2
+#define YX  3
+#define YY  4
+#define YZ  5
+#define ZX  6
+#define ZY  7
+#define ZZ  8
+#define PI  3.141592653589793238462643383279502884197169
 
+#define HUBBLE  0.10 /*Hubble constant in AREPO units*/
 /*
   Program to read AREPO snapshots and compute for each cell:
   - Eigenvalues and Eigenvectors of the velocitiy shear tensor.
@@ -22,12 +34,16 @@
 
 */
 
+int Diagonalise3x3(double *M,double *L,double *vec);
 float * load_data_block(hid_t file, char * block_name, int *n_points, int *n_cols);
 int main(int argc, char **argv){
   hid_t file;
   float *gas_gradient;
   int n_points, n_cols;
-  float shear
+  double shear_tensor[9];
+  double eigenvalues[3];
+  double eigenvectors[9];
+  int i,j;
 
   if(argc!=2){
     fprintf(stderr, "USAGE:%s\n", USAGE);
@@ -38,6 +54,26 @@ int main(int argc, char **argv){
   fprintf(stdout, "opening file %s\n", argv[1]);
 
   gas_gradient = load_data_block(file, GAS_GRADIENT, &n_points, &n_cols);  
+
+  for(i=0;i<n_points;i++){   
+    shear_tensor[XX] = -(gas_gradient[i*9 + XX] + gas_gradient[i*9 + XX]); 
+    shear_tensor[XY] = -(gas_gradient[i*9 + YY] + gas_gradient[i*9 + YY]); 
+    shear_tensor[XZ] = -(gas_gradient[i*9 + ZZ] + gas_gradient[i*9 + ZZ]); 
+    shear_tensor[XY] = -(gas_gradient[i*9 + XY] + gas_gradient[i*9 + YX]); 
+    shear_tensor[XZ] = -(gas_gradient[i*9 + XZ] + gas_gradient[i*9 + YZ]); 
+    shear_tensor[YZ] = -(gas_gradient[i*9 + YZ] + gas_gradient[i*9 + ZY]); 
+    shear_tensor[YX] = shear_tensor[XY];
+    shear_tensor[ZX] = shear_tensor[XZ];
+    shear_tensor[ZY] = shear_tensor[YZ];
+    for(j=0;j<9;j++){
+      shear_tensor[j] *= 1.0/(2.0*HUBBLE);
+    }
+
+    /*Get the Eigenvalues and eigenvectors*/
+    Diagonalise3x3(shear_tensor, eigenvalues,eigenvectors);
+
+  }
+
  
   return 0;
 }
@@ -123,4 +159,139 @@ float * load_data_block(hid_t file, char * block_name, int *n_points, int *n_col
 }
 
 */
+
+
+
+//Diagonalise 3x3 symetric matrix M, L are the eigenvalues (L[0]>=L[1]>=L[2]) and vec the eigenvectors
+//returns 0 if OK, true if degenerate
+//                   (M0 M1 M2)
+//M = (M0 .. M5) --> (M1 M3 M4)
+//                   (M2 M4 M5)
+int Diagonalise3x3(double *M,double *L,double *vec)
+{
+    double a,b,c,u,v,w;
+    double B,C,D;
+    double p,q,r;
+    double phi;
+    double tmp;
+    int i;
+    
+    a=M[0];u=M[1];v=M[2];
+    b=M[3];c=M[5];w=M[4];
+
+    //if ((a==b)&&(b==c)&&(c==0)) return -1;
+
+    //Compute characteristic polynome
+    B=-(a+b+c);
+    C=a*b+a*c+b*c-u*u-v*v-w*w;
+    D=u*u*c+v*v*b+w*w*a-a*b*c-2*u*v*w;
+
+    //solve 3rd degree equation
+    p=(3.*C-B*B)/9.;
+    q=(2.*B*B*B/27.-B*C/3.+D)/2.;
+    
+    if (q*q+p*p*p>=0) 
+    {
+      L[0]=L[1]=L[2]=0;
+      return -1;
+    }
+    
+   
+    r=((q<0)?-1:1)*sqrt(fabs(p));
+    phi = acos(q/(r*r*r))/3.;
+	
+    r*=2;//B/=3;
+    L[0]=-r*cos(phi)-B/3.;
+    L[1]=r*cos(PI/3-phi)-B/3.;
+    L[2]=r*cos(PI/3+phi)-B/3.;
+
+    
+    
+    if ((L[2])>(L[1])) {tmp=L[1];L[1]=L[2];L[2]=tmp;}
+    if ((L[1])>(L[0])) {tmp=L[1];L[1]=L[0];L[0]=tmp;}
+    if ((L[2])>(L[1])) {tmp=L[1];L[1]=L[2];L[2]=tmp;}
+    
+    //Some special case ...
+
+    //matrix is already diagonal
+    if ((u==0)&&(v==0)&&(w==0))
+    {
+	vec[0]=0;vec[1]=0;vec[2]=0;
+	//associate each axis (x,y and z) with it s eigenvalues
+	for (i=0;i<3;i++)
+	{
+	    if (fabs(L[i])>1.E-8)
+	    {
+		vec[3*i+0]=0;vec[3*i+1]=0;vec[3*i+2]=0;
+		if (fabs((a-L[i])/L[i])<1.E-4) vec[3*i+0]=1;
+		else if (fabs((b-L[i])/L[i])<1.E-4) vec[3*i+1]=1;
+		else if (fabs((c-L[i])/L[i])<1.E-4) vec[3*i+2]=1;
+	    }
+	    else {L[i]=0;vec[3*i+0]=0;vec[3*i+1]=0;vec[3*i+2]=0;}
+	}
+    }
+    
+    //We already have 1 eigenvalue (matrix is block diagonal)
+    else if (((u==0)&&(v==0))||((v==0)&&(w==0))||((u==0)&&(w==0)))
+    {
+      double tmpa,tmpb,tmpu;
+	int tmpd;
+	//Finds which axis are already eigenvector (x,y or z)
+	vec[0]=0;vec[1]=0;vec[2]=0;
+	for (i=0;i<3;i++)
+	{
+	    if (fabs(L[i])>1.E-8)
+	    {
+		vec[3*i+0]=0;vec[3*i+1]=0;vec[3*i+2]=0;
+		//Check if we already have this eigenvector
+		if (fabs((a-L[i])/L[i])<1.E-4) vec[3*i+0]=1;
+		else if (fabs((b-L[i])/L[i])<1.E-4) vec[3*i+1]=1;
+		else if (fabs((c-L[i])/L[i])<1.E-4) vec[3*i+2]=1;
+		//no ...
+		//diagonalise 2x2 block ...
+		else
+		{
+		    if ((u==0)&&(v==0)) {tmpa=b;tmpb=c;tmpu=w;tmpd=1;}
+		    if ((v==0)&&(w==0)) {tmpa=a;tmpb=b;tmpu=u;tmpd=0;}
+		    if ((u==0)&&(w==0)) {tmpa=a;tmpb=c;tmpu=v;tmpd=2;}
+		    if ((tmpa==tmpb)&&(tmpb==0)) 
+			B=C=0;
+		    else
+		    {
+			B=(L[i]+tmpu-tmpa)/(L[i]+tmpu-tmpb);
+			C=1./sqrt(1+B*B);
+		    }
+		    if (tmpd==2)
+		    {
+			vec[3*i]=C;
+			vec[3*i+2]=B*C;
+		    }
+		    else
+		    {
+			vec[3*i+tmpd]=C;
+			vec[3*i+tmpd+1]=B*C;
+		    }
+		}
+	    }
+	    else {L[i]=0;vec[3*i+0]=0;vec[3*i+1]=0;vec[3*i+2]=0;}
+	}
+    }
+
+    //General case ...
+    else for (i=0;i<3;i++)
+    {
+
+	B=L[i]-b-u*u/(L[i]-a);
+	B=(w+u*v/(L[i]-a))/B;
+	
+	C=u/(L[i]-a)*B+v/(L[i]-a);
+	D=1./sqrt(B*B+C*C+1.);
+
+	vec[3*i+2]=D;
+	vec[3*i+1]=B*D;
+	vec[3*i]=C*D;
+    }
+    
+    return 0;
+}
 
