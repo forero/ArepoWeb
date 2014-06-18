74f9e04b2396bd7ab8ed47f7a9488d3e6843607d
Jamie <j.e.forero.romero@gmail.com>
2014-06-18 19:24:54 +0200

    python script to generate runs

--- src/filling.py
+++ src/filling.py
@@ -6,5 +6,6 @@ min_len = max_len/2.0
 lambda_values = 0.01 * (640.0/max_len)**(3.0/2.0)
 
 for i in range(8):
-    run="./snap2fill.x /home/springel/universe/AnalysisIllustris/AddGrads/L75n455FP/snapdir_136/snap_136 ../data/gas_web/gasweb_snap_136 ../data/filling/ILL3_136 %f %f %f"%(min_len[i], max_len[i], lambda_values[i])
-    print run
+    run_fill="./snap2fill.x /home/springel/universe/AnalysisIllustris/AddGrads/L75n455FP/snapdir_136/snap_136 ../data/gas_web/gasweb_snap_136 ../data/filling/ILL3_136 %f %f %f"%(min_len[i], max_len[i], lambda_values[i])
+    run_lambda="./snap2lambda.x /home/springel/universe/AnalysisIllustris/AddGrads/L75n455FP/snapdir_136/snap_136 ../data/gas_web/gasweb_snap_136 ../data/histo/ILL3_136 %f %f %f"%(min_len[i], max_len[i], lambda_values[i])
+    print run_lambda
