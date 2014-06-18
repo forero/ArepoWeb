74847ae0e22551c5ff866a37cdd7521b59b091de
Jamie <j.e.forero.romero@gmail.com>
2014-06-18 09:13:03 +0200

    new snap2web with IO routines outside

--- src/snap2web.c
+++ src/snap2web.c
@@ -3,15 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#define DM_COORDINATES "/PartType1/Coordinates"
-#define DM_VELOCITIES "/PartType1/Velocities"
-#define GAS_GRADIENT "/PartType0/VelocityGradient"
-#define GAS_VELOCITIES "/PartType0/Velocities"
-#define GAS_DENSITY "/PartType0/Density"
-#define EIGENVECTORS "/ShearTensor/Eigenvectors"
-#define EIGENVALUES "/ShearTensor/Eigenvalues"
-#define VORTICITY "/ShearTensor/Vorticity"
-#define HELICITY "/ShearTensor/Helicity"
+#include "io_arepo.h"
 #define USAGE "./snap2web.x snapfilename fileout"
 #define XX  0
 #define XY  1
@@ -39,13 +31,11 @@
   
   Author: Jaime Forero-Romero (Uniandes)
   Written: 12-June-2014
-  Modified: 
+  Modified: 18-June-2014 (factored out IO routines into a different file)
 
 */
 
 int Diagonalise3x3(double *M,double *L,double *vec);
-float * load_data_block(hid_t file, char * block_name, int *n_points, int *n_cols);float *malloc_data(int n_points, int n_cols);
-void dump_data_block(hid_t file, char * block_name, float *data, int n_points, int n_cols);
 int main(int argc, char **argv){
   char fileout[512];
   hid_t file, grp;
@@ -144,58 +134,6 @@ float *malloc_data(int n_points, int n_cols){
   return data;
 }
 
-void dump_data_block(hid_t file, char * block_name, float *data, int n_points, int n_cols){
-  hid_t dataspace, status, dataset, plist;
-  hsize_t dims[2];
-
-
-  dims[0] = n_points;
-  dims[1] = n_cols;
-  dataspace = H5Screate_simple(2, dims, NULL); 
-
-  plist     = H5Pcreate(H5P_DATASET_CREATE);
-  H5Pset_chunk(plist, 2, dims);
-  dataset = H5Dcreate(file, block_name, 
-		      H5T_NATIVE_FLOAT, dataspace, H5P_DEFAULT, plist, H5P_DEFAULT);
-  status = H5Dwrite(dataset, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL,
-		    H5P_DEFAULT, data);
-
-
-  status = H5Dclose(dataset);
-  status = H5Pclose(plist);
-  status = H5Sclose(dataspace);
-}
-
-float * load_data_block(hid_t file, char * block_name, int *n_points, int *n_cols){
-  hid_t dset, filespace, status_n, memspace;
-  int rank;
-  hsize_t dims[2];
-  long long n_items;
-  float *data;
-
-  /*READ DM coordinates*/
-  dset = H5Dopen2(file, block_name, H5P_DEFAULT);
-  fprintf(stdout, "getting dataset %s\n", block_name);
-  filespace = H5Dget_space (dset);
-
-  rank = H5Sget_simple_extent_ndims (filespace);
-  fprintf(stdout, "rank is: %d\n", rank);
-
-  status_n = H5Sget_simple_extent_dims (filespace, dims, NULL);
-  fprintf(stdout, "dimesions are: %d %d\n", (int)(dims[0]), (int)(dims[1]));
-
-  *n_points = dims[0];
-  *n_cols = dims[1];
-
-  n_items = (long long)dims[0] * (long long)dims[1];
-  if(!(data=malloc(sizeof(float) * n_items))){
-    fprintf(stderr, "problem with data allocation\n");
-  }
-  memspace = H5Screate_simple (rank,dims,NULL);  
-  status_n = H5Dread (dset, H5T_NATIVE_FLOAT, memspace, filespace,
-		    H5P_DEFAULT, data);
-  return data;
-}
 
 /*
   group = H5Gopen(file, "/Header", H5P_DEFAULT);
