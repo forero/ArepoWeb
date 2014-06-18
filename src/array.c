0032d70e0f9ca57ae17b0dceeedf5220237c7031
Jamie <j.e.forero.romero@gmail.com>
2014-06-18 16:21:30 +0200

    array routines to get filling fractions

--- src/array.c
+++ src/array.c
@@ -1,5 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include "io_arepo.h"
+#include "struct.h"
+
+
+void add_to_environments(double *total_env, double *lambda_th, float *cargo, float *eigenvalues, int n_lambda, int n_points, int env_kind, float *volume, float min_len, float max_len){
+  int i, j;
+  float my_len;
+
+  for(i=0;i<n_points;i++){
+    my_len = pow((3.0*volume[i]/(4.0*PI)), 1.0/3.0);
+    if((my_len>min_len) && (my_len<max_len)){
+      for(j=0;j<n_lambda;j++){      
+	if(env_kind==VOID){
+	  if(eigenvalues[i*3 + EIGEN1] < lambda_th[j]){
+	    total_env[j] +=cargo[j];
+	  }
+	}else if(env_kind==SHEET){
+	  if((eigenvalues[i*3 + EIGEN2] < lambda_th[j]) && (eigenvalues[i*3 + EIGEN1]>lambda_th[j])){
+	    total_env[j] +=cargo[j];
+	  }
+	}else if(env_kind==FILAMENT){
+	  if((eigenvalues[i*3 + EIGEN3] < lambda_th[j]) && (eigenvalues[i*3 + EIGEN2]>lambda_th[j])){
+	    total_env[j] +=cargo[j];
+	  }
+	}else if(env_kind==PEAK){
+	  if(eigenvalues[i*3 + EIGEN3]>lambda_th[j]){
+	    total_env[j] +=cargo[j];
+	  }
+	}else{
+	  fprintf(stderr, "ERROR: uknown kind of environment\n");
+	  exit(1);
+	}
+      }
+    }
+  }
+}
 
 void dump_histo(char *filename, double*histo, double*edges, int n_histo){
   FILE *out;
@@ -11,7 +48,8 @@ void dump_histo(char *filename, double*histo, double*edges, int n_histo){
   }
   
   for(i=0;i<n_histo;i++){
-    dummy = 0.5*(edges[i] + edges[i+1]);
+    //    dummy = 0.5*(edges[i] + edges[i+1]);
+    dummy = edges[i];
     fprintf(out, "%f %f\n", dummy, histo[i]);
   }  
   fclose(out);
--- src/array.h
+++ src/array.h
@@ -1,6 +1,7 @@
 #ifndef __ARRAY_H__
 #define __ARRAY_H__
 
+void add_to_environments(double *total_env, double *lambda_th, float *cargo, float *eigenvalues, int n_lambda, int n_points, int env_kind, float *volume, float min_len, float max_len);
 void dump_histo(char *filename, double *histo, double *edges, int n_histo);
 double *gen_linspace(double min_val, double max_val, int n_points, double *delta_val);
 void  add_to_histo(double *histo, double *edges, int n_histo, float *data, int n_data);
